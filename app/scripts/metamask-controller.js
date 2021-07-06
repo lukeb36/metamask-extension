614e2d492e6668dd2f578b753b95f3daa65ca9b1
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2021-07-06 18:24:11 -0400

    convertoing chainId to decimal and using dynamic list in detectNewToken

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -24,7 +24,7 @@ import {
   CurrencyRateController,
   PhishingController,
   NotificationController,
-  TokenListController
+  TokenListController,
 } from '@metamask/controllers';
 import { TRANSACTION_STATUSES } from '../../shared/constants/transaction';
 import { MAINNET_CHAIN_ID } from '../../shared/constants/network';
@@ -32,6 +32,7 @@ import { UI_NOTIFICATIONS } from '../../shared/notifications';
 import { toChecksumHexAddress } from '../../shared/modules/hexstring-utils';
 import { MILLISECOND } from '../../shared/constants/time';
 
+import { hexToDecimal } from '../../ui/helpers/utils/conversions.util';
 import ComposableObservableStore from './lib/ComposableObservableStore';
 import AccountTracker from './lib/account-tracker';
 import createLoggerMiddleware from './lib/createLoggerMiddleware';
@@ -186,8 +187,8 @@ export default class MetamaskController extends EventEmitter {
       name: 'TokenListController',
     });
     this.tokenListController = new TokenListController({
-      chainId: this.networkController.getCurrentChainId(),
-      onNetworkStateChange: this.networkController.store.subscribe.bind(this.networkController.store),
+      chainId: hexToDecimal(this.networkController.getCurrentChainId()),
+      onNetworkStateChange: this._onModifiedNetworkStateChange.bind(this),
       messenger: tokenListMessenger,
       state: initState.tokenListController,
     });
@@ -306,7 +307,7 @@ export default class MetamaskController extends EventEmitter {
       preferences: this.preferencesController,
       network: this.networkController,
       keyringMemStore: this.keyringController.memStore,
-      tokenList: this.tokenListController
+      tokenList: this.tokenListController,
     });
 
     this.addressBookController = new AddressBookController(
@@ -574,7 +575,18 @@ export default class MetamaskController extends EventEmitter {
     );
     return providerProxy;
   }
-
+  _onModifiedNetworkStateChange = (cb) => {
+    this.networkController.store.subscribe(async (networkState) => {
+      const modifiedNetworkState = {
+        ...networkState,
+        provider: {
+          ...networkState.provider,
+          chainId: hexToDecimal(networkState.provider.chainId),
+        },
+      };
+      return await cb(modifiedNetworkState);
+    });
+  };
   /**
    * TODO:LegacyProvider: Delete
    * Constructor helper: initialize a public config store.
