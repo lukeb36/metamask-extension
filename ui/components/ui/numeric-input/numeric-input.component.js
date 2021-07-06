13ee8dcf977b6bb2c4b7d489a3c3781f0a73837e
David Walsh <davidwalsh83@gmail.com>
2021-07-06 12:01:30 -0500

    Prevent leading zeros in numeric inputs

--- ui/components/ui/numeric-input/numeric-input.component.js
+++ ui/components/ui/numeric-input/numeric-input.component.js
@@ -18,7 +18,7 @@ export default function NumericInput({
       <input
         type="number"
         value={value}
-        onChange={(e) => onChange?.(Number(e.target.value))}
+        onChange={(e) => onChange?.(parseInt(e.target.value, 10))}
         min="0"
         autoFocus={autoFocus}
       />
